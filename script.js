33138536295557c969f6e65d6050b64245a5f700
Gast <gast@Gasts-MacBook-Pro.local>
2021-01-15 20:21:51 +0100

    updating functions and changing css style

--- script.js
+++ script.js
@@ -1,91 +1,99 @@
-//Dom variables, span because it is in a span, div because it is a div
-//caching - storing for the future use; so no need to write "document.getEle" so many times
-let userScore =0;
-let compScore=0;
-const userScore_span = document.getElementById("user_score");
-const compScore_span = document.getElementById("comp_score");
-const scoreBoard_div = document.querySelector(".score_board");
-const result_p = document.querySelector(".result >p");
-const rock_div = document.getElementById("st");
-const paper_div = document.getElementById("pa");
-const scissors_div = document.getElementById("sc");
+// Caching DOM variables - storing for the future use; so no need to write "document.getEle" all over again
 
-function getCompChoice(){
-	const choices = ['st', 'pa', 'sc'];
-	//we need N nb <0;3) -> 0,1,2
-	const randomNb = Math.floor(Math.random() * 3);
-	return choices [randomNb];
+
+let userScore = 0;
+let compScore = 0;
+const userScore_span = document.getElementById("user-score");
+const compScore_span = document.getElementById("comp-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_p = document.querySelector(".result > p");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+
+function getCompChoice() {
+	const choices = ['r', 'p', 's'];
+	const randomNumber = Math.floor(Math.random() * 3);
+	return choices[randomNumber];
 }
 
-function convertToWord(letter){
-	if (letter === "st") return "Stone";
-	if (letter === "pa") return "Paper";
+function convertToWord(letter) {
+	if (letter == r) return "Rock";
+	if (letter == p) return "Paper";
 	return "Scissors";
-	
 }
 
-function win(userChoice, compChoice){
+function win(userChoice, compChoice) {
+	const smallUserWord = "user".fontsize(4).sub();
+	const smallCompWord = "comp".fontsize(4).sub();
+	const userChoice_div = document.getElementById(userChoice);
 	userScore++;
 	userScore_span.innerHTML = userScore;
 	compScore_span.innerHTML = compScore;
-	result_p.innerHTML = `convertToWord(userChoice) beats convertToWord(compChoice). :fire You Win! :fire"`;
+	result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} beats ${convertToWord(compChoice)}${smallCompWord} You win!`; 
+	userChoice_div.classList.add('green-border');
+	userChoice_div.classList.add('green-border:hover');
+	setTimeout(() => {userChoice_div.classList.remove('green-border')}, 1000);
+	setTimeout(() => {userChoice_div.classList.remove('green-border:hover')}, 1000);
 }
 
-function lose(){
+function lose(userChoice, compChoice) {
+	const smallUserWord = "user".fontsize(4).sub();
+	const smallCompWord = "comp".fontsize(4).sub();
+	const userChoice_div = document.getElementById(userChoice);
 	compScore++;
-	
+	userScore_span.innerHTML = userScore;
+	compScore_span.innerHTML = compScore;
+	result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} loses to ${convertToWord(compChoice)}${smallCompWord} You lose!`;
+	userChoice_div.classList.add('red-border');
+	userChoice_div.classList.add('red-border:hover');
+	setTimeout(() => {userChoice_div.classList.remove('red-border')}, 1000);
+	setTimeout(() => {userChoice_div.classList.remove('red-border:hover')}, 1000); 
 }
 
-function draw(){
-	
-	
+function draw(userChoice, compChoice) {
+	const smallUserWord = "user".fontsize(4).sub();
+	const smallCompWord = "comp".fontsize(4).sub();
+	const userChoice_div = document.getElementById(userChoice);
+	result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} equals ${convertToWord(compChoice)}${smallCompWord} It's draw!`; 
+	userChoice_div.classList.add('grey-border');
+	userChoice_div.classList.add('grey-border:hover');
+	setTimeout(() => {userChoice_div.classList.remove('grey-border')}, 1000);
+	setTimeout(() => {userChoice_div.classList.remove('grey-border:hover')}, 1000); 
 }
 
-function game(userChoice){
+function game(userChoice) {
 	const compChoice = getCompChoice();
-	
+
 	switch (userChoice + compChoice) {
-		
-	case "stsc";
-	case "past";
-	case "scpa";
-		win(userChoice, compChoice);
+
+		case "rs":
+		case "sp":
+		case "pr":
+		 win(userChoice, compChoice);
 		break;
-		
-	case "stpa";
-	case "pasc";
-	case "scst";
-		lose(userChoice, compChoice));
+
+		case "sr":
+		case "ps":
+		case "rp":
+		 lose(userChoice, compChoice);
 		break;
-		
-	case "stst";
-	case "papa";
-	case "scsc";
-		draw(userChoice, compChoice));
+
+		case "rr":
+		case "pp":
+		case "ss":
+		 draw(userChoice, compChoice);
 		break;
-		
 	}
-	
+
 }
 
-function main(){
-	
-rock_div.addEventListener('click', function() {
-	game("st");
-	
-});
+function main() {
+   rock_div.addEventListener('click', () => game("r"));
+  
+   paper_div.addEventListener('click', () => game("p"));
 
-paper_div.addEventListener('click', function() {
-	game("pa");
-	
-});
-
-scissors_div.addEventListener('click', function() {
-	game("sc");
-	
-});
+   scissors_div.addEventListener('click', () => game("s"));
+}
 
 main();
-
-
-}
